ac2b589ea29ffb70cf7ce8a923dfe6b3650caa84
Kartik-91 <kartikbanwalaop@gmail.com>
2022-10-22 16:51:34 +0530

    pissarro: update to 13.0.3 SKTMIXM

---
+++ Android.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2022 Paranoid Android
+# Copyright (C) 2022 Beru Hinode
+#
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: GPL-2.0-only
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(strip pissarro $(TARGET_DEVICE)),)
+
+$(info Including firmware for pissarro...)
+
+FIRMWARE_IMAGES := $(wildcard $(LOCAL_PATH)/images/*)
+
+$(foreach f, $(notdir $(FIRMWARE_IMAGES)), \
+    $(call add-radio-file,images/$(f)))
+
+endif
